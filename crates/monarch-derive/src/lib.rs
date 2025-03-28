e0396a70fca1ffa159f9391bd6e830638c295a7f
Michael Ciraci <michaelciraci@hotmail.com>
2025-03-27 21:51:11 -0500

    Autogenerating primes

--- crates/monarch-derive/src/lib.rs
+++ crates/monarch-derive/src/lib.rs
@@ -22,6 +22,7 @@ const COPRIMES: [(usize, usize); 11] = [
     (5, 6),
 ];
 const MIXED_RADIX: [(usize, usize); 1] = [(5, 5)];
+const PRIMES: [usize; 8] = [5, 7, 11, 13, 17, 19, 23, 29];
 
 fn _compute_twiddle<T: Float + FloatConst>(index: usize, fft_len: usize) -> Complex<T> {
     let constant = T::from(-2.0).unwrap() * T::PI() / T::from(fft_len).unwrap();
@@ -243,6 +244,196 @@ pub fn generate_mixed_radix(_input: TokenStream) -> TokenStream {
             }
         }
     });
+
+    let expanded = quote! {
+        #(#ss)*
+    };
+    proc_macro::TokenStream::from(expanded)
+}
+
+#[proc_macro]
+pub fn generate_primes(_input: TokenStream) -> TokenStream {
+    let ss = PRIMES.clone().into_iter().map(|s| {
+        let func = Ident::new(&format!("fft{}", s), Span::call_site().into());
+        let halflen = (s + 1) / 2;
+        let twiddles = (1..halflen).map(|n| {
+            let var = Ident::new(&format!("twiddle{}", n), Span::call_site().into());
+            let val: Complex<f64> = _compute_twiddle(n, s);
+            let re = val.re;
+            let im = val.im;
+            quote! {
+                let #var = Complex::new(T::from(#re).unwrap(), T::from(#im).unwrap());
+            }
+        });
+        let first_codegen = (1..halflen).map(|n| {
+            let var1 = Ident::new(&format!("x{}{}p", n, s - n), Span::call_site().into());
+            let var2 = Ident::new(&format!("x{}{}n", n, s - n), Span::call_site().into());
+            quote! {
+                let #var1 = x[#n] + x[#s - #n];
+                let #var2 = x[#n] - x[#s - #n];
+            }
+        });
+        let second_codegen = (1..halflen).map(|n| {
+            let var = Ident::new(&format!("x{}{}p", n, s - n), Span::call_site().into());
+            quote! {
+                + #var
+            }
+        });
+        let third_codegen = (1..halflen).map(|n| {
+            let var1 = Ident::new(&format!("b{}{}re_a", n, s - n), Span::call_site().into());
+            let sub1 = (1..halflen).map(|m| {
+
+                let mut mn = (m * n) % s;
+                if mn > s / 2 {
+                    mn = s - mn;
+                }
+                let var2 = Ident::new(&format!("twiddle{}", mn), Span::call_site().into());
+                let var3 = Ident::new(&format!("x{}{}p", m, s - m), Span::call_site().into());
+                quote! {
+                    + #var2.re * #var3.re
+                }
+            });
+
+            quote! {
+                let #var1 = x[0].re #(#sub1)* ;
+            }
+        });
+
+        let fourth_codegen = (1..halflen).map(|n| {
+            let var1 = Ident::new(&format!("b{}{}re_b", n, s - n), Span::call_site().into());
+            let sub1 = (1..halflen).map(|m| {
+
+                let mut mn = (m * n) % s;
+                if mn > s / 2 {
+                    mn = s - mn;
+                    let var2 = Ident::new(&format!("twiddle{}", mn), Span::call_site().into());
+                    let var3 = Ident::new(&format!("x{}{}n", m, s - m), Span::call_site().into());
+                    quote! {
+                        - #var2.im * #var3.im
+                    }
+                } else {
+                    let var2 = Ident::new(&format!("twiddle{}", mn), Span::call_site().into());
+                    let var3 = Ident::new(&format!("x{}{}n", m, s - m), Span::call_site().into());
+                    quote! {
+                        + #var2.im * #var3.im
+                    }
+                } 
+                
+            });
+
+            quote! {
+                let #var1 = T::zero() #(#sub1)* ;
+            }
+        });
+        let fifth_codegen = (1..halflen).map(|n| {
+            let var1 = Ident::new(&format!("b{}{}im_a", n, s - n), Span::call_site().into());
+            let sub1 = (1..halflen).map(|m| {
+
+                let mut mn = (m * n) % s;
+                if mn > s / 2 {
+                    mn = s - mn;
+                }
+                let var2 = Ident::new(&format!("twiddle{}", mn), Span::call_site().into());
+                let var3 = Ident::new(&format!("x{}{}p", m, s - m), Span::call_site().into());
+                quote! {
+                    + #var2.re * #var3.im
+                }
+            });
+
+            quote! {
+                let #var1 = x[0].im #(#sub1)* ;
+            }
+        });
+
+        let sixth_codegen = (1..halflen).map(|n| {
+            let var1 = Ident::new(&format!("b{}{}im_b", n, s - n), Span::call_site().into());
+            let sub1 = (1..halflen).map(|m| {
+
+                let mut mn = (m * n) % s;
+                if mn > s / 2 {
+                    mn = s - mn;
+                    let var2 = Ident::new(&format!("twiddle{}", mn), Span::call_site().into());
+                    let var3 = Ident::new(&format!("x{}{}n", m, s - m), Span::call_site().into());
+                    quote! {
+                        - #var2.im * #var3.re
+                    }
+                } else {
+                    let var2 = Ident::new(&format!("twiddle{}", mn), Span::call_site().into());
+                    let var3 = Ident::new(&format!("x{}{}n", m, s - m), Span::call_site().into());
+                    quote! {
+                        + #var2.im * #var3.re
+                    }
+                } 
+                
+            });
+
+            quote! {
+                let #var1 = T::zero() #(#sub1)* ;
+            }
+        });
+
+        let seventh_codegen = (1..s).map(|n| {
+            let mut nfold = n;
+            if n > s / 2 {
+                nfold = s - n;
+            
+                let var1 = Ident::new(&format!("out{}re", n), Span::call_site().into());
+                let var2 = Ident::new(&format!("out{}im", n), Span::call_site().into());
+                let var3 = Ident::new(&format!("b{}{}re_a", nfold, s-nfold), Span::call_site().into());
+                let var4 = Ident::new(&format!("b{}{}re_b", nfold, s-nfold), Span::call_site().into());
+                let var5 = Ident::new(&format!("b{}{}im_a", nfold, s-nfold), Span::call_site().into());
+                let var6 = Ident::new(&format!("b{}{}im_b", nfold, s-nfold), Span::call_site().into());
+
+                quote! {
+                    let #var1 = #var3 + #var4;
+                    let #var2 = #var5 - #var6;
+                }
+            } else {
+                let var1 = Ident::new(&format!("out{}re", n), Span::call_site().into());
+                let var2 = Ident::new(&format!("out{}im", n), Span::call_site().into());
+                let var3 = Ident::new(&format!("b{}{}re_a", nfold, s-nfold), Span::call_site().into());
+                let var4 = Ident::new(&format!("b{}{}re_b", nfold, s-nfold), Span::call_site().into());
+                let var5 = Ident::new(&format!("b{}{}im_a", nfold, s-nfold), Span::call_site().into());
+                let var6 = Ident::new(&format!("b{}{}im_b", nfold, s-nfold), Span::call_site().into());
+                quote!{
+                    let #var1 = #var3 - #var4;
+                    let #var2 = #var5 + #var6;
+                }
+            }
+        });
+        let eigth_codegen = (1..s).map(|n| {
+            let var_re: Ident = Ident::new(&format!("out{}re", n), Span::call_site().into());
+            let var_im: Ident = Ident::new(&format!("out{}im", n), Span::call_site().into());
+
+            quote! {
+                Complex::new(#var_re, #var_im),
+            }
+        });
+
+        quote! {
+            #[inline]
+            pub fn #func<T: Float + FloatConst, A: AsRef<[Complex<T>]>>(input: A) -> [Complex<T>; #s] {
+                let n = #s;
+                let x = input.as_ref();
+                assert_eq!(n, x.len());
+
+                #(#twiddles)*
+
+                #(#first_codegen)*
+                let sum = x[0] #(#second_codegen)* ;
+                #(#third_codegen)*
+                #(#fourth_codegen)*
+                #(#fifth_codegen)*
+                #(#sixth_codegen)*
+                #(#seventh_codegen)*
+
+                [
+                    sum,
+                    #(#eigth_codegen)*
+                ]
+            }
+
+    }});
 
     let expanded = quote! {
         #(#ss)*
