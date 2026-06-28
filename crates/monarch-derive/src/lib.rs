42d62aa61efb37a7b91394f84b63a60891d88b1d
Michael Ciraci <michaelciraci@hotmail.com>
2026-06-28 13:27:37 -0400

    WIP

--- crates/monarch-derive/src/lib.rs
+++ crates/monarch-derive/src/lib.rs
@@ -22,7 +22,18 @@ use syn::Ident;
 //     198, 199, 200,
 // ];
 
-const SIZES: [usize; 26] = [2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 28, 32];
+const SIZES: [usize; 197] = [
+    2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
+    28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
+    76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99,
+    100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118,
+    119, 120, 121, 122, 123, 124, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138,
+    139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157,
+    158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176,
+    177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195,
+    196, 197, 198, 199, 200,
+];
 
 const HAND_GEN: [usize; 1] = [3];
 
@@ -36,7 +47,10 @@ enum FFTType {
 
 impl FFTType {
     fn compute_type(n: usize) -> FFTType {
-        if n.is_power_of_two() {
+        // TODO
+        if n == 27 {
+            FFTType::Coprime
+        } else if n.is_power_of_two() {
             FFTType::PowerOfTwo
         } else {
             // Check if it has an integer square root
@@ -107,11 +121,11 @@ pub fn generate_switch(_input: TokenStream) -> TokenStream {
 
     let expanded = quote! {
         /// Top level FFT function
-        /// 
+        ///
         /// ```
         /// use monarch_butterfly::*;
         /// use num_complex::Complex;
-        /// 
+        ///
         /// let mut output = vec![Complex::new(0.0, 0.0); 8];
         /// let input: Vec<_> = (0..8).map(|i| Complex::new(i as f32, 0.0)).collect();
         /// fft::<8, _, _>(input, output.as_mut_slice());
@@ -203,7 +217,7 @@ pub fn generate_powers_of_two(_input: TokenStream) -> TokenStream {
     });
 
     let expanded = quote! {
-        
+
         #[inline(always)]
         pub fn fft1<T: Float, A: AsRef<[Complex<T>]>>(input: A, output: &mut [Complex<T>]) {
             let n = 1;
@@ -701,6 +715,7 @@ mod tests {
     #[test]
     fn test_fft_type() {
         assert_eq!(FFTType::compute_type(25), FFTType::Mixed);
+        assert_eq!(FFTType::compute_type(27), FFTType::Coprime);
         assert_eq!(FFTType::compute_type(36), FFTType::Coprime);
     }
 }
